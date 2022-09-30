9f8b3c24d0740685fc37861d33bfeea18b6c5ac2
theRobMay <robbie.maysent@gmail.com>
2022-09-29 21:13:50 -0500

    updated login function

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,6 +1,8 @@
 let db = require("../util/db");
 //this will help w storing password hash and checking it
 let argon2 = require("argon2");
+//token signature lib
+let jwt = require("jsonwebtoken");
 
 //we expect the body to contain an object that contains the username and password and full name
 //{"username": "doobiehouser", "password": "lol420", "full_name": "Doug Blazer"}
@@ -34,7 +36,45 @@ let sql = "insert into people (username, password_hash, full_name) values (?,?,?
 
 }
 let login = function (req,res) {
+    let username = req.body.username;
+    let password = req.body.password;
 
+    let sql = "select username, password_hash, full_name from people where username = ?"
+    let params = [username]
+    db.query(sql, params, async function (err, rows) {
+        if (err) {
+            console.log("could not get hash", err);
+            res.sendStatus(500);
+        } else {
+            if (rows.length > 1) {
+                console.log("returned too many rows", username);
+                res.sendStatus(500);
+            } else if (rows.length == 0) {
+                res.sendStatus(400);
+            } else {
+                let pwhash = rows[0].password_hash;
+                let fName = rows[0].full_name;
+
+                let pass = false
+                try {
+                    pass = await argon2.verify(pwhash, password);
+                } catch (err) {
+                    console.log("failed to verify pass", err);
+                }
+                if (pass){
+                    let token = {
+                        "fullName": fName,
+                    };
+                    //sign the token, send signed token
+                    let signedToken = jwt.sign(token, process.env.JWT_SECRET)
+                    res.json(signedToken)
+
+                } else {
+                    res.sendStatus(400);
+                }
+            }
+        }
+    })
 }
 
 module.exports = {
