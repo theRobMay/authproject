c802d7477effa4fa17385215152f5559e042733e
theRobMay <robbie.maysent@gmail.com>
2022-10-11 20:32:17 -0500

    working w flyctl

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ app.use(express.json());
 
 let messageRoutes = require("./routes/messageRoutes");
 let authRoutes = require("./routes/authRoutes");
+const path = require("path");
 
 app.use(messageRoutes);
 app.use(authRoutes);
@@ -15,3 +16,4 @@ let port = process.env.PORT || 8080
 app.listen(port, function (){
 
 })
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/src/public.html')));
